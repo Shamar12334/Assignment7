c3e1414c460a64e45e7f10354af4aea7cedd44df
shamar <weekesshamar38@gmail.com>
2025-03-19 09:39:25 -0400

    Worked in integer merge sort file setting up to methods for merge sort and merge

--- src/assignment/IntegerMergeSort.java
+++ src/assignment/IntegerMergeSort.java
@@ -1,4 +1,18 @@
 package assignment;
 
 public class IntegerMergeSort {
+    public void mergeSort(int [] A, int num1, int num2)
+    {
+        if (num1 >= num2)
+            return;
+        int midNum= (num1+num2)/2;// return so you can implement a floor
+        mergeSort(A,num1,midNum);
+        mergeSort(A,midNum+1,num2);
+        merge(A,num1,midNum,num2);
+    }
+    public int merge(int [] A, int num1,int mid,int num2)
+    {
+        //length of array
+        int n= mid-num1+num2;
+    }
 }
