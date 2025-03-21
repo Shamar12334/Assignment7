75f908526e4d3eb2320a327f313af79895e1d441
Shamar <weekesshamar38@gmail.com>
2025-03-20 22:39:54 -0500

    Added comments and structured the code for readability

--- src/assignment/IntegerMergeSort.java
+++ src/assignment/IntegerMergeSort.java
@@ -1,13 +1,18 @@
 package assignment;
 
-public class IntegerMergeSort {
+public class IntegerMergeSort
+{
     public void mergeSort(int [] arr, int lowerBound, int upperBound)
     {
+        // if the lower-bound is less than the upper-bound there is nothing in the array
         if (lowerBound >= upperBound)
             return;
-        int midNum= (lowerBound+upperBound)/2;// return so you can implement a floor
+        int midNum= (int) Math.floor((lowerBound+upperBound)/2);
+        //sort the first half
         mergeSort(arr,lowerBound,midNum);
+        //sort the second half
         mergeSort(arr,midNum+1,upperBound);
+        // merges the separate and sorted arrays back together
         merge(arr,lowerBound,midNum,upperBound);
     }
     public void merge(int [] arr, int lowerBound,int mid,int upperBound)
@@ -32,21 +37,25 @@ public class IntegerMergeSort {
         int k = lowerBound;
         while (i < leftArrayLength && j < rightArrayLength)
         {
-            if(leftArray[i] < rightArray[j]){
+            if(leftArray[i] < rightArray[j])
+            {
                 arr[k] = leftArray[i];
                 i +=1;
-            }else{
+            }else
+            {
                 arr[k] = rightArray[j];
                 j+=1;
             }
             k+=1;
         }
+        //if i is less than left array length the loop will execute
         while (i< leftArrayLength)
         {
             arr[k]= leftArray[i];
             i+=1;
             k+=1;
         }
+        // if j is less than right array length the loop will execute
         while(j < rightArrayLength)
         {
             arr[k]= rightArray[j];
