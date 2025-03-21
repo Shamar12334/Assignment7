2918bafe14493f61fe3b0e4d9c83bee84ee5cf17
Shamar <weekesshamar38@gmail.com>
2025-03-20 22:27:58 -0500

    Set up the integerMergeSort file to conduct mergesort and merge on integers

--- src/assignment/IntegerMergeSort.java
+++ src/assignment/IntegerMergeSort.java
@@ -1,18 +1,55 @@
 package assignment;
 
 public class IntegerMergeSort {
-    public void mergeSort(int [] A, int num1, int num2)
+    public void mergeSort(int [] arr, int lowerBound, int upperBound)
     {
-        if (num1 >= num2)
+        if (lowerBound >= upperBound)
             return;
-        int midNum= (num1+num2)/2;// return so you can implement a floor
-        mergeSort(A,num1,midNum);
-        mergeSort(A,midNum+1,num2);
-        merge(A,num1,midNum,num2);
+        int midNum= (lowerBound+upperBound)/2;// return so you can implement a floor
+        mergeSort(arr,lowerBound,midNum);
+        mergeSort(arr,midNum+1,upperBound);
+        merge(arr,lowerBound,midNum,upperBound);
     }
-    public int merge(int [] A, int num1,int mid,int num2)
+    public void merge(int [] arr, int lowerBound,int mid,int upperBound)
     {
         //length of array
-        int n= mid-num1+num2;
+        int leftArrayLength = mid-lowerBound+1;
+        int rightArrayLength = upperBound-mid;
+        // arrays
+        int []  rightArray= new int[rightArrayLength];
+        int [] leftArray= new int[leftArrayLength];
+        //new arrays getting filled
+        for(int i :  leftArray)
+        {
+            leftArray[i] = arr[lowerBound+i];
+        }
+        for(int j : rightArray)
+        {
+            rightArray[j]= arr[mid+j+1];
+        }
+        int i = 0;
+        int j = 0;
+        int k = lowerBound;
+        while (i < leftArrayLength && j < rightArrayLength)
+        {
+            if(leftArray[i] < rightArray[j]){
+                arr[k] = leftArray[i];
+                i +=1;
+            }else{
+                arr[k] = rightArray[j];
+                j+=1;
+            }
+            k+=1;
+        }
+        while (i< leftArrayLength)
+        {
+            arr[k]= leftArray[i];
+            i+=1;
+            k+=1;
+        }
+        while(j < rightArrayLength)
+        {
+            arr[k]= rightArray[j];
+        }
     }
 }
